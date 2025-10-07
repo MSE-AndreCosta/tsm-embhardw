f780e573ec25a28f88423092656f45914cc96e64
André <andre_miguel_costa@hotmail.com>
2025-10-07 17:14:24 +0200

    feat: add timer to design

--- lab01/software/led_counter/counter.c
+++ lab01/software/led_counter/counter.c
@@ -10,26 +10,23 @@
 #include <stdio.h>
 #include "system.h"
 
+volatile unsigned long counter;
+
+void timer interrupt(void *context, alt u32 id){
+	counter++;
+	IOWR_8DIRECT(PARALLELPORT_0_BASE, 0x8, counter);
+	IOWR_16DIRECT(TIMER_0_BASE, 0, 0x0);
+}
+
 int main() {
 	printf("Let's start counting\n");
-	int counter = 0;
-#if 0
-	IOWR_8DIRECT(LEDS_BASE, 0, 0);
-	while(1)
-	{
-		counter++;
-		printf("counter = %2d\n", counter);
-		IOWR_8DIRECT(LEDS_BASE, 0, counter);
-	}
-#endif
+
 	IOWR_32DIRECT(PARALLELPORT_0_BASE, 0x0, 0xFF);
 	IOWR_32DIRECT(PARALLELPORT_0_BASE, 0x8, 0);
 
-	while(1)
-	{
-		counter++;
-		printf("counter = %2d\n", counter);
-		IOWR_8DIRECT(PARALLELPORT_0_BASE, 0x8, counter);
-	}
+	alt_irq_register(TIMER 0 IRQ,(void*)2,(alt isr func)timer interrupt);
+	IOWR_16DIRECT(TIMER 0 BASE, 4, 0x7);
+
+	while(1) ;
 
 }
