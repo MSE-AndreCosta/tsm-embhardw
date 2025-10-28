7b9b9575b59470f03e80b9e20ab2829f2434c44f
André <andre_miguel_costa@hotmail.com>
2025-10-28 13:38:34 +0100

    fix: don't control leds in timer irq

--- dma/software/lcd-dma/timer.c
+++ dma/software/lcd-dma/timer.c
@@ -9,7 +9,7 @@ static void timer_isr(void *context, alt_u32 id);
 static void timer_isr(void *context, alt_u32 id)
 {
 	counter++;
-	IOWR_32DIRECT(PARALLEL_PORT_0_BASE, 0x8, counter);
+
 	IOWR_16DIRECT(TIMER_0_BASE, 0, 0x0);
 }
 void timer_init(void)
